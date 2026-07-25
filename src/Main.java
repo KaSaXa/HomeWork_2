df2ade50984b35b64c3d48cdd15061b476bb3029
KaSaXa <akurmelevv@gmail.com>
2026-07-26 00:06:28 +0300

    Task_7

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var FirstFighter = 78.2;
-        var SecondFighter = 82.7;
-        var AllMass = FirstFighter + SecondFighter;
-             System.out.println(AllMass);
-        var Difference = SecondFighter - FirstFighter;
-              System.out.println(Difference);
+        var firstFighter = 78.2;
+        var secondFighter = 82.7;
+        var last = secondFighter % firstFighter;
+            System.out.println(last);
 }
     }
